9d067debca6bba2e7e3f5f45ffaf7a4726ee8e8d
Chely Ho <adept_inept@yahoo.com>
2018-09-29 19:38:33 +0000

    automatically backed up by learn

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -6,7 +6,11 @@ export default function shoppingListItemReducer(
 ) {
 	switch (action.type) {
 		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+			console.log('Current state.items length %s', state.items.length);
+			console.log('Updating state.items length to %s', state.items.length + 1)
+			return Object.assign({}, state, {
+				items: state.items.concat(state.items.length + 1)
+			})
 
 		default:
 			return state;
